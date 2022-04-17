09694319ef89cd484609b7a55234df87d60e229b
chenstarx <chenstarx@outlook.com>
2022-04-17 14:47:10 -0700

    feat: lab7

---
+++ hw7/lab7_c_2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#define NAME "lab7_c"
+#define IN  pidx[0]
+#define OUT pidx[1]
+
+#define BUF_LEN 3
+
+long n = 2;
+
+int main(int argc, char *argv[])
+{
+    int shm_fd;
+    float *arr;
+    pid_t pid;
+
+    if (argc != 2)
+    {
+        printf("Please provide a valid argument\n");
+        return 1;
+    }
+
+    n = strtol(argv[1], NULL, 10);
+
+    if (!(n > 0))
+    {
+        printf("Please provide a valid argument larger than 0\n");
+        return 1;
+    }
+
+    printf("Buffer len: %d, n: %ld\n", BUF_LEN, n);
+    printf("Address of n: %p\n", &n);
+
+    // open a shared memory segment
+    shm_fd = shm_open(NAME, O_CREAT | O_RDWR, 0666);
+
+    // limit the size
+    int buf_size_byte = BUF_LEN * sizeof(float) + 2 * sizeof(int);
+    ftruncate(shm_fd, buf_size_byte);
+
+    // map the shared memory to current process address space
+    arr = mmap(0, buf_size_byte, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    
+    printf("Start address of shared buffer: %p\n", &arr);
+
+    // get the pointer for IN and OUT
+    int* pidx = (int*) &arr[BUF_LEN];
+
+    // init IN and OUT
+    IN = 0;
+    OUT = 0;
+    
+    printf("Producer start\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        while (((IN + 1) % BUF_LEN) == OUT);
+
+        float sent = 0.25 * i * i;
+
+        printf("Producer sending %f\n", sent);
+        arr[i%BUF_LEN] = sent;
+        IN = (IN + 1) % BUF_LEN;
+
+        int nanosec = rand() % 5000000;
+
+        printf("Producer sleeping %.3fs\n", (float) nanosec / 1000000.0);
+        usleep(nanosec);
+
+    }
+
+    printf("Producer finished\n");
+
+    return 0;
+}
